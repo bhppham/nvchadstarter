77a130a5ac0b429e63bf1c14377fd3b3b8fd6fc1
bhppham <252704436go@gmail.com>
2025-09-02 15:12:41 -0400

    Added love2d library

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,40 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.config('lua_ls', {
+  on_init = function (client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'Lua 5.3',
+      },
+      completion = {
+        callSnippet = 'Replace',
+      },
+      workspace = {
+        library = {
+          '${3rd}/love2d/library',
+        },
+      },
+      diagnostics = {
+        globals = { 'vim', 'love' },
+      },
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+})
+
+local servers = {}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
+--
